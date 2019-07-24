78787b2190f3d61b008a18082769059233abb531
John Gliksberg <jg.trosh@gmail.com>
2019-07-24 10:59:18 +0200

    Display minutes left during work time

--- pomo.py
+++ pomo.py
@@ -4,13 +4,15 @@ from subprocess import run
 from shlex import split
 from time import sleep
 
-worktime = 20 * 60
+worktime = 20 # Minutes
 duration = 60
 minsleep = 0.2
 maxsleep = 2
 nflashes = abs(duration/minsleep - duration/maxsleep) // 2
 
-sleep(worktime)
+for minute in range(worktime):
+    print(f"{worktime - minute} minutes left")
+    sleep(60)
 for i in range(int(nflashes)):
     run(split("xrefresh -solid black"))
     sleep(minsleep + (maxsleep - minsleep) * i / nflashes)
